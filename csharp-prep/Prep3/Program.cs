7f70a77c30ab6cf62b59bd6ae81a4ffb6bf4223b
JesNiexyz <jessednielson@gmail.com>
2025-09-22 09:47:45 -0600

    loop review

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomnumber = new Random();
+        int magicNumber = randomnumber.Next(1,101);
+
+        int guessedNumber;
+
+        do
+        {
+            Console.WriteLine("Guess the magic number ");
+            guessedNumber = int.Parse(Console.ReadLine());
+
+            if (guessedNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+
+            }
+            else if (guessedNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+
+            }
+            else
+            {
+                Console.WriteLine("Correct");
+
+            }
+        } while (guessedNumber != magicNumber);
+
     }
 }
